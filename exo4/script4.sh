39f5f9ca9d2faf542024b8e658dde8abc0cccb30
MolenGeek <f.chekili@outlook.com>
2023-11-02 11:41:32 +0100

    rajout de l'exo 4

---
+++ exo4/script4.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mkdir -p public/{css/{footer,header,main},img,js} && touch index.html public/css/footer/style.css public/css/header/style.css public/css/main/style.css public/img/img1 public/img/img2 public/js/main.js
